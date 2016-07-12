6a529afc69763b301d2bf047bbcf561e7c1b2d62
rongolberg <ron.goblerg@icloud.com>
2016-07-12 14:20:22 +0300

    fixed indexer

--- duck-proj/duck/elastic/indexer.py
+++ duck-proj/duck/elastic/indexer.py
@@ -3,6 +3,7 @@ import json
 from elasticsearch import Elasticsearch
 import docx2txt
 
+
 def connect(host='10.0.0.2', port=9677):
     """
     the function will connect to a provided elastic search indexer
