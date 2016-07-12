7bf02bc830c205bac3f2b5898b174b9dcee2a82d
rongolberg <ron.goblerg@icloud.com>
2016-07-12 14:20:04 +0300

    fixed searcher

--- duck-proj/duck/elastic/indexer.py
+++ duck-proj/duck/elastic/indexer.py
@@ -3,16 +3,30 @@ import json
 from elasticsearch import Elasticsearch
 import docx2txt
 
+def connect(host='10.0.0.2', port=9677):
+    """
+    the function will connect to a provided elastic search indexer
+    :param host: ip of the elastic search host
+    :type host: str
+    :param port: port of the elastic search host
+    :type port: int
+    :return: elastic search object
+    :rtype: Elasticsearch object
+    """
+    es = Elasticsearch([{'host': host, 'port': port}])
+    return es
 
-def index(path):
+
+def index(connection, path='/Users/rongolberg/PycharmProjects/Duck/DuckDuckGo/wiki'):
     """
-    the function will iterate through a given folder and return a Json for each file
-    :param path:
-    :type path:
-    :return:
-    :rtype:
+    the function will index a folder of docs in the provided elastic search host ip and port
+    :param connection: the host connection
+    :type connection: Elasticsearch object
+    :param path: path we want to index
+    :type path: str
+    :return: None
+    :rtype: None
     """
-    es = Elasticsearch([{'host': '10.0.0.2', 'port': 9677}])
     id = 0
     for i in os.listdir(path):
         if i.endswith(".docx"):
@@ -22,13 +36,5 @@ def index(path):
             dic = {'path': file_path, 'doc': raw_doc}
             json_dict = json.dumps(dic)
             print json_dict
-            es.index(index='wiki', doc_type="article", id=id, body=dic)
+            connection.index(index='wiki', doc_type="article", id=id, body=dic)
             print i
-
-
-def main():
-    index('/Users/rongolberg/PycharmProjects/Duck/DuckDuckGo/wiki')
-
-
-if __name__ == '__main__':
-    main()
